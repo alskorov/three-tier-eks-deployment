ec15aa72793c2103910bb83dfffab116cdb3c319
alskorov <skorovalex@gmail.com>
2024-11-29 00:39:52 +0200

    disabled Jenknis server spinup

--- terraform/13-security-group.tf
+++ terraform/13-security-group.tf
@@ -1,31 +1,31 @@
-resource "aws_security_group" "security-group" {
-  vpc_id      = aws_vpc.main.id
+# resource "aws_security_group" "security-group" {
+#   vpc_id      = aws_vpc.main.id
   
-  description = "Allowing Jenkins, Sonarqube, SSH Access, ArgoCD"
+#   description = "Allowing Jenkins, Sonarqube, SSH Access, ArgoCD"
 
-  ingress = [
-    for port in [22, 8080, 9000, 9090, 80] : {
-      description      = "TLS from VPC"
-      from_port        = port
-      to_port          = port
-      protocol         = "tcp"
-      ipv6_cidr_blocks = []
-      self             = false
-      prefix_list_ids  = []
-      security_groups  = []
-      #restrict access to your IP or 0.0.0.0/0 to ALL
-      cidr_blocks      = ["87.70.172.247/32"]
-    }
-  ]
+#   ingress = [
+#     for port in [22, 8080, 9000, 9090, 80] : {
+#       description      = "TLS from VPC"
+#       from_port        = port
+#       to_port          = port
+#       protocol         = "tcp"
+#       ipv6_cidr_blocks = []
+#       self             = false
+#       prefix_list_ids  = []
+#       security_groups  = []
+#       #restrict access to your IP or 0.0.0.0/0 to ALL
+#       cidr_blocks      = ["87.70.172.247/32"]
+#     }
+#   ]
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = local.sg-name
-  }
-}
+#   tags = {
+#     Name = local.sg-name
+#   }
+# }
